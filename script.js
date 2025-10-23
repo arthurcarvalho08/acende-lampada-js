bbb9625733c423e5e3628139a77a5bfdcf755412
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-10-23 14:54:01 +0000

    quase no fim

---
+++ script.js
@@ -0,0 +1,9 @@
+function acender(){
+    document.getElementById("lampada").src="on.png"
+    console.log("Luz acesa.")
+}
+
+function apagar(){
+        document.getElementById("lampada").src="off.png"
+        console.log("Luz apagada.")
+}
